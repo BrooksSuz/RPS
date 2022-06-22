8d423289916c54cb86b0b95787c0c3f7b905b5d3
Brooks Susor <susorbrooks@gmail.com>
2022-06-22 15:17:07 -0400

    Finish rules; begin game function

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 //variables
-const playerSelection = "Rock";
+const playerSelection = 'rock';
 const computerSelection = computerPlay();
 
 function computerPlay() {
-  let rockPaperScissors = ['Rock', 'Paper', 'Scissors']; 
+  let rockPaperScissors = ['rock', 'paper', 'scissors']; 
   return rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)]; 
 }; 
 
-const playRound = (playerSelection, computerSelection) => {
+function playRound() {
   //losing scenarios
-  if (playerSelection.toLowerCase === 'rock' && computerSelection.toLowerCase === 'paper') {
+  if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
     return 'You lose! Paper beats Rock!'; 
   }
-  else if (playerSelection.toLowerCase === 'paper' && computerSelection.toLowerCase === 'scissors') {
+  else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') {
     return 'You lose! Scissors beat Paper!'; 
   }
-  else if (playerSelection.toLowerCase === 'scissors' && computerSelection.toLowerCase === 'rock') {
+  else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'rock') {
     return 'You lose! Rock beats scissors!'; 
   }
 
   //winning scenarios
-  else if (playerSelection.toLowerCase === 'rock' && computerSelection.toLowerCase === 'scissors') {
+  else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
     return 'You win! Rock beats Scissors!'; 
   }
-  else if (playerSelection.toLowerCase === 'paper' && computerSelection.toLowerCase === 'rock') {
+  else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
     return 'You win! Paper beats Rock!'; 
   }
-  else if (playerSelection.toLowerCase === 'scissors' && computerSelection.toLowerCase === 'paper') {
+  else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper') {
     return 'You win! Scissors beat Paper!'; 
   }
-  else if (playerSelection.toLowerCase() === computerSelection.toLowerCase) {
+
+  //tieing scenario
+  else {
     return `It's a tie!`; 
   }
 }
 
-//console.log(playRound(playerSelection, computerSelection));
+const game = () => {
+  return playRound(playerSelection, computerSelection); 
+}
+
+console.log(game());
