83976ccaba321205c69a346de86d258c2605c54d
Brooks Susor <susorbrooks@gmail.com>
2022-06-23 12:49:44 -0400

    Continued to edit playRound function

--- script.js
+++ script.js
@@ -7,28 +7,33 @@ function computerPlay() {
   return rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)]; 
 }; 
 
-function playRound() {
+function playRound(playerSelection, computerSelection) {
   //losing scenarios
-  playerSelection === 'rock' && computerSelection === 'paper' ? 'You lose! Paper beats Rock!' 
-  : playerSelection === 'paper' && computerSelection === 'scissors' ? 'You lose! Scissors beat Paper!' 
-  : playerSelection === 'scissors' && computerSelection === 'rock' ? 'You lose! Rock beats scissors!' 
+  playerSelection === 'rock' && computerSelection === 'paper' 
+  ? 'You lose! Paper beats Rock!' 
+  : playerSelection === 'paper' && computerSelection === 'scissors' 
+  ? 'You lose! Scissors beat Paper!' 
+  : playerSelection === 'scissors' && computerSelection === 'rock' 
+  ? 'You lose! Rock beats scissors!' 
 
   //winning scenarios
-  : playerSelection === 'rock' && computerSelection === 'scissors' ? 'You win! Rock beats Scissors!' 
-  : playerSelection === 'paper' && computerSelection === 'rock' ? 'You win! Paper beats Rock!' 
-  : playerSelection === 'scissors' && computerSelection === 'paper' ? 'You win! Scissors beat Paper!'
+  : playerSelection === 'rock' && computerSelection === 'scissors' 
+  ? 'You win! Rock beats Scissors!' 
+  : playerSelection === 'paper' && computerSelection === 'rock' 
+  ? 'You win! Paper beats Rock!' 
+  : playerSelection === 'scissors' && computerSelection === 'paper' 
+  ? 'You win! Scissors beat Paper!'
 
   //error message on incorrect input
-  : playerSelection !== 'rock' || 'paper' || 'scissors' ? 'Incorrect input. Type \'Rock\', \'Paper\', or \'Scissors\'.' 
+  : playerSelection !== 'rock' || 'paper' || 'scissors' 
+  ? 'Incorrect input. Type \'Rock\', \'Paper\', or \'Scissors\'.' 
 
   //tieing scenario
   : `It's a tie!`; 
 }
 
 const game = () => {
-  for (let i = 0; i < 5; i++) {
-    return playRound(playerSelection, computerSelection); 
-  }
+  return playRound(); 
 }
 
 console.log(game());
